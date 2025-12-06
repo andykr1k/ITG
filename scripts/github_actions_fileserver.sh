6de0a86362fe023161aad5d0da065441f31b3ebc
andykr1k <akrikorian12@gmail.com>
2025-12-06 04:48:49 -0500

    Added github action scripts and changed launch files

--- scripts/github_actions_fileserver.sh
+++ scripts/github_actions_fileserver.sh
@@ -12,4 +12,4 @@ echo "Building new image..."
 sudo docker build -t filestorage .
 
 echo "Starting container..."
-sudo docker run -d -p 18000:8000 --name filestorage filestorage
+sudo docker run -d -p 18888:8888 --name filestorage filestorage
