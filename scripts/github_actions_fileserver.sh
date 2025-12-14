718428a8996086691f5aaf65a0839dd53042c950
andykr1k <akrikorian12@gmail.com>
2025-12-13 21:45:59 -0500

    Changed workflow

--- scripts/github_actions_fileserver.sh
+++ scripts/github_actions_fileserver.sh
@@ -5,11 +5,11 @@ echo "Pulling latest changes..."
 git pull
 
 echo "Stopping existing container..."
-sudo docker stop filestorage || true
-sudo docker rm filestorage || true
+docker stop filestorage || true
+docker rm filestorage || true
 
 echo "Building new image..."
-sudo docker build -t filestorage .
+docker build -t filestorage .
 
 echo "Starting container..."
-sudo docker run -d -p 18888:8888 --name filestorage filestorage
+docker run -d -p 18888:8888 --name filestorage filestorage
--- scripts/launch_server_docker.sh
+++ scripts/launch_server_docker.sh
@@ -1,6 +1,6 @@
 cd ../server
 docker stop server || true
 docker rm server || true
-sudo docker build -t server .
-sudo docker run -d -p 18000:8000 server
+docker build -t server .
+docker run -d -p 18000:8000 server
 cd ../scripts
